2b4cbb90afea9848d111879a3cd5df5a4a753a2b
kngwyu <yuji.kngw.80s.revive@gmail.com>
2017-06-25 22:40:38 +0900

    modify README

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,6 @@ fn main() {
                 match player {
                     &mut PlayerType::CommandAI(ref mut cmd) => {
                         let s = app.game.get_state_str(id);
-                        println!("{:?}", s);
                         cmd.write(s);
                         let act = cmd.act();
                         app.game.act(id, act);
